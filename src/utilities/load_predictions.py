239ecd085532f324af42fccb6298a005dfe4ec52
Vaillus <hugovaillaud@gmail.com>
2025-07-02 16:27:11 +0200

    added a function to load only the predictions where the ground truth is not none

--- src/utilities/load_predictions.py
+++ src/utilities/load_predictions.py
@@ -1,7 +1,9 @@
 from typing import Union
+import numpy as np
 
 from src.utilities.paths import get_pred_dir
 from src.utilities.pixel_angle_converter import angles_to_pixels
+from src.utilities.load_ground_truth import read_ground_truth_angles
 
 
 def load_predictions(video_index, frame_index=None, predictions_dir="5") -> Union[tuple, list]:
@@ -29,3 +31,18 @@ def load_predictions(video_index, frame_index=None, predictions_dir="5") -> Unio
         return pred_pixels[frame_index]
     else:
         return pred_pixels
+    
+def load_filtered_predictions(video_index, frame_index=None, predictions_dir="5") -> Union[tuple, list]:
+    """
+    Load predictions for the video and convert them to pixel coordinates.
+    Filter out predictions where the ground truth (angles) is NaN.
+    """
+    predictions = load_predictions(video_index, frame_index, predictions_dir)
+    gt = read_ground_truth_angles(video_index)
+    
+    # Filter predictions where gt is not NaN
+    filtered_predictions = []
+    for pred, gt_angle in zip(predictions, gt):
+        if not (np.isnan(gt_angle[0]) or np.isnan(gt_angle[1])):
+            filtered_predictions.append(pred)
+    return filtered_predictions
